a17cf69486ce35337030cf0e2bd2e088bfc5dd82
JANGMEN <joknek654@naver.com>
2023-03-29 18:26:46 +0900

    20230329 수요일 Basic1
    20230329 수요일 Basic1

--- studyProject/src/Day01/Basic01.java
+++ studyProject/src/Day01/Basic01.java
@@ -21,11 +21,39 @@ public class Basic01 {
 		// 실수형 : float, double ex) 1.0, 1.34
 		// 논리형 : boolean ex) true, false(참, 거짓)
 		
+		/*
 		int a = 15;
 		int b = 16;
 		
-		System.out.println(a);
+		System.out.println(a); // 콘솔창에 결과값을 보여줌
 		System.out.println(b);
+		
+		System.out.println(a + b); // 덧셈
+		System.out.println(a - b); // 뺄셈
+		System.out.println(a * b); // 곱셈
+		System.out.println(a / b); // 나눗셈(몫), 정수 / 정수 = 정수, 정수 / 실수 = 실수
+		System.out.println(a % b); // 나머지 */
+		
+		/* 문제 1) int a = 124일 경우 각 자리 수의 합을 구하시오. (100 ~ 999)
+		int a = 124;
+		
+		int a1 = (a % 10); // 마지막 자리 수 구하기
+		int a2 = (a / 10 % 10); // 두 번째 자리 수 구하기
+		int a3 = (a / 100); // 첫 번째 자리 수 구하기
+		
+		System.out.println(a1 + a2 + a3); // 세 자리 합 구하기 */
+		
+		// 문제 2) 1000~9999의 숫자를 뒤집어서 출력하시오. ex) 2451 => 1542
+		int a = 2451;
+		
+		int a1 = a % 10;// 마지막 자리 수 구하기
+		int a2 = a / 10 % 10; // 세 번째 자리 수 구하기
+		int a3 = a / 100 % 10;// 두 번째 자리 수 구하기
+		int a4 = a / 1000;// 첫 번째 자리 수 구하기
+		
+		int a5 = (a1*1000) + (a2*100) + (a3*10) + a4;
+		System.out.println(a5);
+		
 
 	}
 
