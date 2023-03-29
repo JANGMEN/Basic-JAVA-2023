5bc8808c12754d38c9afaeb058997164df0ff7a1
JANGMEN <joknek654@naver.com>
2023-03-29 18:08:09 +0900

    20230329 테스트222
    222

--- studyProject/src/Day01/Basic01.java
+++ studyProject/src/Day01/Basic01.java
@@ -1,10 +1,31 @@
 package Day01;
 
+// 한 줄 주석문 사용
+/*여
+ 러
+ 줄
+ 주
+ 석
+ */
+// 폰트 크기 변경 : ctrl + 더하기, 빼기
+// 클래스명 첫번째 문자는 대문자 ex) Student Car StudentClass
+// 변수명 소문자 ex) car student studentAge
+// 메소드(함수)명 ex) printMyAge
+
 public class Basic01 {
 
 	public static void main(String[] args) {
 		// 타입 변수명 = 초기화값
-		// byte, short, int(32bit), long(64bit)
+		// 정수형 : byte, short, int(32bit), long(64bit) ex) 3, -1
+		// 문자형 : char ex) 'a', '1', '0', 'N'
+		// 실수형 : float, double ex) 1.0, 1.34
+		// 논리형 : boolean ex) true, false(참, 거짓)
+		
+		int a = 15;
+		int b = 16;
+		
+		System.out.println(a);
+		System.out.println(b);
 
 	}
 
